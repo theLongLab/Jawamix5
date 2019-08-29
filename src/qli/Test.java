ae34e5e2b2dde46faedf0d1a53c0eda0b43208a4
Qing Li <liqingbioinfo@gmail.com>
2019-08-29 10:14:30 -0600

    qing add a package qli to store her code

---
+++ src/qli/Test.java
@@ -0,0 +1,11 @@
+package qli;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Morning seems very hard for others to arrive, and the same to me.");
+
+	}
+
+}
